a03ec88202ecc14f896eeef3478183b45e4fb545
Jyo61 <53649847+Jyo61@users.noreply.github.com>
2021-05-30 20:57:01 +0530

    Second commit

--- mongosee.js
+++ mongosee.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/cakeshop', {
     useNewUrlParser: true, 
     useUnifiedTopology: true}).then(function(){
-        console.log("connection successful.")
+        console.log("DB connection successful.")
     }).catch(function(error){
         console.log("something went wrong",error)
     });
+    
