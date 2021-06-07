efabb873297c3b39759131fdc87a3f30d22fd322
Jyo61 <53649847+Jyo61@users.noreply.github.com>
2021-06-08 01:41:11 +0530

    commit

---
+++ models/todo.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose')
+
+var TodoitemSchema = new mongoose.Schema({
+
+    title : {
+        type : String,
+        required : true,
+        maxlength : 32,
+        trim : true 
+    },
+    completed:{
+        type : Boolean
+    },
+    Duedate : {
+               type : Date,
+               required : True
+    }
+})
+
+module.exports = mongoose.model("Todo",TodoitemSchema)
